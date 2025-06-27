acf84b1f66ac20f4a35f9545c2948f0e7bf6bfa1
Daiane Camargo <dkeelly@gmail.com>
2025-06-27 13:37:55 -0300

    finalização pag dash
    finalizei os dois blocos finais da pagina

--- Site Completo/script/dashboard.js
+++ Site Completo/script/dashboard.js
@@ -8,7 +8,7 @@ function clickMenu() {
     }
 }
 
-// SELECT O ANO NO CALENDÁRIO
+// SELECT O ANO NO CALENDÁRIO - QUANTIDADE MENSAL
 function popularAnos() {
   const selectAno = document.getElementById('ano');
   const anoAtual = new Date().getFullYear();
@@ -24,3 +24,37 @@ function popularAnos() {
 
 // Chama a função para popular os anos quando a página carregar
 document.addEventListener('DOMContentLoaded', popularAnos);
+
+// SELECT O ANO NO CALENDÁRIO - CO2
+function popularAnos1() {
+  const selectAno = document.getElementById('ano1');
+  const anoAtual = new Date().getFullYear();
+  const anoInicial = anoAtual - 100; // Define um intervalo de 100 anos para trás
+
+  for (let i = anoInicial; i <= anoAtual; i++) {
+    const option = document.createElement('option');
+    option.value = i;
+    option.text = i;
+    selectAno.add(option);
+  }
+}
+
+// Chama a função para popular os anos quando a página carregar
+document.addEventListener('DOMContentLoaded', popularAnos1);
+
+// SELECT O ANO NO CALENDÁRIO - QUANTIDADE ANUAL
+function popularAnos2() {
+  const selectAno = document.getElementById('ano2');
+  const anoAtual = new Date().getFullYear();
+  const anoInicial = anoAtual - 100; // Define um intervalo de 100 anos para trás
+
+  for (let i = anoInicial; i <= anoAtual; i++) {
+    const option = document.createElement('option');
+    option.value = i;
+    option.text = i;
+    selectAno.add(option);
+  }
+}
+
+// Chama a função para popular os anos quando a página carregar
+document.addEventListener('DOMContentLoaded', popularAnos2);
